d5a07a5cdcccc2a1ec15ffd65c4d449b49877b26
Maciej Myszke <maciej.myszke@umlaut.com>
2020-08-10 13:46:17 +0200

    Add header

---
+++ include/ara/core/span.h
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2020
+ * umlaut Software Development and contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef ARA_CORE_SPAN_H_
+#define ARA_CORE_SPAN_H_
+
+// forward declarations
+class Span;
+
+/**
+ * A view over a contiguous sequence of objects.
+ *
+ *
+ * @req {SWS_CORE_01901}
+ * @req {SWS_CORE_01900}
+ * @req {SWS_CORE_01911}
+ * @req {SWS_CORE_01912}
+ * @req {SWS_CORE_01913}
+ * @req {SWS_CORE_01914}
+ * @req {SWS_CORE_01921}
+ * @req {SWS_CORE_01915}
+ * @req {SWS_CORE_01916}
+ * @req {SWS_CORE_01917}
+ * @req {SWS_CORE_01918}
+ * @req {SWS_CORE_01919}
+ * @req {SWS_CORE_01920}
+ * @req {SWS_CORE_01931}
+ * @req {SWS_CORE_01941}
+ * @req {SWS_CORE_01942}
+ * @req {SWS_CORE_01943}
+ * @req {SWS_CORE_01944}
+ * @req {SWS_CORE_01945}
+ * @req {SWS_CORE_01946}
+ * @req {SWS_CORE_01947}
+ * @req {SWS_CORE_01948}
+ * @req {SWS_CORE_01949}
+ * @req {SWS_CORE_01950}
+ * @req {SWS_CORE_01951}
+ * @req {SWS_CORE_01952}
+ * @req {SWS_CORE_01961}
+ * @req {SWS_CORE_01962}
+ * @req {SWS_CORE_01963}
+ * @req {SWS_CORE_01964}
+ * @req {SWS_CORE_01965}
+ * @req {SWS_CORE_01966}
+ * @req {SWS_CORE_01967}
+ * @req {SWS_CORE_01968}
+ * @req {SWS_CORE_01969}
+ * @req {SWS_CORE_01970}
+ * @req {SWS_CORE_01971}
+ * @req {SWS_CORE_01972}
+ * @req {SWS_CORE_01973}
+ * @req {SWS_CORE_01974}
+ * @req {SWS_CORE_01975}
+ * @req {SWS_CORE_01976}
+ * @req {SWS_CORE_01977}
+ * @req {SWS_CORE_01978}
+ * @req {SWS_CORE_01979}
+ * @req {SWS_CORE_01990}
+ * @req {SWS_CORE_01991}
+ * @req {SWS_CORE_01992}
+ * @req {SWS_CORE_01993}
+ * @req {SWS_CORE_01994}
+ * /
